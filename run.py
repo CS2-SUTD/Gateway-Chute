68676553ea3f85f2c61c85eac6cfe44b4f6b1bd0
cs2 <cs2.projs@gmail.com>
2024-07-10 16:55:56 +0800

    improve multistream performance

--- run.py
+++ run.py
@@ -8,30 +8,30 @@ import time
 # Add a global variable to track if Ctrl+C has been pressed
 ctrl_c_pressed = False
 
-def start_process(cmd):
+def start_process(cmd, cpu):
     try:
         process = subprocess.Popen(cmd)
     except subprocess.SubprocessError as e:
         print(f'Failed to start {cmd}: {e}')
         sys.exit(1)
     p = psutil.Process(process.pid)
-    # p.cpu_affinity(cpu)
+    p.cpu_affinity(cpu)
     return process
 
 # Start the scripts
-process1 = start_process(['python', 'main.py', '-c', 'config/config1.ini', '-s', 'rtsp://192.168.0.157:8554/chute'])
-process2 = start_process(['python', 'main.py', '-c', 'config/config2.ini', '-s', 'rtsp://192.168.0.157:8554/chute'])
-process3 = start_process(['python', 'main.py', '-c', 'config/config3.ini', '-s', 'rtsp://192.168.0.157:8554/chute'])
+process1 = start_process(['python', 'main.py', '-c', 'config/config1.ini', '-s', 'data/sample.mp4'], [0,1])
+process2 = start_process(['python', 'main.py', '-c', 'config/config2.ini', '-s', 'data/sample.mp4'], [2,3])
+process3 = start_process(['python', 'main.py', '-c', 'config/config3.ini', '-s', 'data/sample.mp4'], [4,5])
 
 def signal_handler(sig, frame):
     global ctrl_c_pressed
     ctrl_c_pressed = True
     print('Ctrl+C detected. Terminating scripts...')
     os.kill(process1.pid, signal.SIGTERM)
-    os.kill(process2.pid, signal.SIGTERM)
+    # os.kill(process2.pid, signal.SIGTERM)
     os.kill(process3.pid, signal.SIGTERM)
     process1.wait()
-    process2.wait()
+    # process2.wait()
     process3.wait()
     print('All processes have been terminated.')
     sys.exit(0)
